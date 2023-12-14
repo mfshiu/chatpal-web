a4a88bef7d83bdb9fe62dc989a82db8aed725d4f
mfshiu <avatar.xu@gmail.com>
2023-12-14 15:07:55 +0800

    ..

--- app.py
+++ app.py
@@ -3,29 +3,78 @@
  
 from datetime import datetime
 from flask import Flask, jsonify, redirect, render_template, request, url_for
+from logging import Logger
+import os
+import queue
+from threading import Thread
 
-from book import book_app
-import helper
 import paho.mqtt.client as mqtt
-from setting import setting_app
+
+import helper
 import app_config
- 
-       
+
+
+logger:Logger = helper.get_logger() 
+response_queue = queue.Queue()
+mqtt_client = None
+mqtt_thread = None
+
+
+def on_connect(client, userdata, flags, rc):
+    logger.info("Connected with result code " + str(rc))
+    client.subscribe("voice.text")
+    client.subscribe("voice.url")
+
+def on_message(client:mqtt.Client, db, message):
+    global response_queue
+    logger.debug(f"Topic: {message.topic}, Payload: {message.payload}")
+    text = message.payload.decode('utf-8', 'ignore')
+    if "voice.text" == message.topic:
+        logger.info(f"Reply text: {text}")
+    elif "voice.url" == message.topic:
+        response_queue.put(text)
+        logger.info(f"Reply voice: {text}, response_queue: {response_queue}")
+        client.publish("voice.spoken")
+
+
+def initialize_mqtt():
+    logger.debug(f"initialize_mqtt")
+    global mqtt_client, mqtt_thread
+
+    if mqtt_client:
+        logger.info(f"MQTT is initialized.")
+        return
+    
+    mqtt_client = mqtt.Client()
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_message = on_message
+
+    mqtt_params = app_config.mqtt_params
+    mqtt_client.username_pw_set(mqtt_params["username"], mqtt_params["password"])
+    mqtt_client.connect(mqtt_params["host"], mqtt_params["port"], mqtt_params["keepalive"])
+
+    def run_mqtt():
+        mqtt_client.loop_start()
+    mqtt_thread = Thread(target=run_mqtt)
+    mqtt_thread.start()
+    
+
 app = Flask(__name__) 
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-app.register_blueprint(book_app)
-app.register_blueprint(setting_app)
-
-# MQTT Configuration
-mqtt_params = app_config.mqtt_params
-mqtt_client = mqtt.Client()
-mqtt_client.username_pw_set(mqtt_params["username"], mqtt_params["password"])
-mqtt_client.connect(mqtt_params["host"], mqtt_params["port"], mqtt_params["keepalive"])
+initialize_mqtt()
 
        
+def cleanup_mqtt():
+    """ Clean up MQTT client resources. """
+    global mqtt_client
+    if mqtt_client:
+        mqtt_client.loop_stop()
+        mqtt_client.disconnect()
+
+
 @app.context_processor
 def inject_stage_and_region():  
-    return dict(title="vChat 管理系統")
+    return dict(title="Chat Pal")
    
                
 @app.route('/')
@@ -34,43 +83,41 @@ def home():
     return render_template('index.html')
     # return render_template('test/test_kdavis_vad.html')
     # return redirect(url_for('booking.book'))
-    
+     
 
 @app.route('/upload_voice', methods=['POST'])
 def upload_voice():
     if 'audio_data' in request.files:
         audio_file = request.files['audio_data']
        
-        # To read the file directly
         audio_content = audio_file.read()
-        print(f"publish:'hearing.voice', audio_content: {len(audio_content)}")
+        # logger.debug(f"publish:'hearing.voice', audio_content: {len(audio_content)}")
         mqtt_client.publish("hearing.voice", audio_content)
-    #     cmd_insert = f'''insert into "AudioInput"
-    # (wave_data) values (%s) RETURNING id;'''
-    #     params = (audio_content,)
-    #     audio_id = helper.insert_and_fetch_id(cmd_insert, params)
-    #     print(f"audio_id: {audio_id}")
-     
-        # Alternatively, to save the file and then read it
-        # filepath = f"_input/received_audio_{datetime.now().strftime('%Y%m%d-%H%M%S')}.wav"
-        # filepath = f"/Users/xumingfang/Work/chatpal/web/_input/received_audio_{datetime.now().strftime('%Y%m%d-%H%M%S')}.wav"
-        filepath = f"/Users/xumingfang/Work/Temp/received_audio_{datetime.now().strftime('%Y%m%d-%H%M%S')}.wav"
-        # audio_file.save(filepath)
-        # with open(filename, 'rb') as f:
-        #     audio_content = f.read()
+        filepath = os.path.join(app_config.audio_temp_dir, f"received_audio_{datetime.now().strftime('%Y%m%d-%H%M%S')}.wav")
         with open(filepath, 'wb') as f: 
             f.write(audio_content)
-   
-        return jsonify({'message': 'Audio processed successfully'})
+     
+        try:
+            global response_queue
+            logger.debug(f"Wait for responsing")
+            response = response_queue.get(timeout=10)  # Timeout in seconds
+            logger.debug(f"response: {response}")
+            return jsonify({"audioUrl": response})
+        except queue.Empty:
+            logger.error("queue.Empty")
+            return jsonify({"error": "No response received within timeout"}), 408
+        # return jsonify({'message': 'Audio processed successfully'})
     else:
         return jsonify({'error': 'No audio file provided'}), 400
          
-     
+           
 @app.route('/app-user-list.html')
 def userList():
     return render_template('/app-user-list.html')
  
-   
+    
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+    # initialize_mqtt()
     #app.run(host=config.app_ip, port=config.app_port)
